34d36db7249bd50da4a2bc18edcacece976eb382
Sajan Lawrence <sajanlawrence12345@gmail.com>
2025-09-24 22:55:24 +0530

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Sajan Lawrence on 24/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
